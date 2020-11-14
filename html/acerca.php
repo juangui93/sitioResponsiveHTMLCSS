da20434500e3cbe9650132a3ea3312006002ccc7
Juan Guillermo Del Valle Urrego <juangui93@gmail.com>
2020-11-13 19:59:35 -0500

    Se crea sitio web responsive con HTML y se aplican estilos en CSS

---
+++ html/acerca.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juan Guillermo Del Valle Urrego</title>
+     <!--Estilo para desktop-->
+     <link href="../css/desktop.css" rel="stylesheet" type="text/css" media="only screen and (min-width:1025px)" />
+
+    <!--Estilo para tablet-->
+    <link href="../css/tablet.css" rel="stylesheet" type="text/css" media="only screen and (min-width:768px) and (max-width:1024px)" />
+
+    <!--Estilo para movil-->
+    <link href="../css/movil.css" rel="stylesheet" type="text/css" media="only screen and (min-width:280px) and (max-width:767px)" />
+</head>
+<body>
+    <header>
+        <img src="../img/cabecera1.png" id="header">
+   </header>
+
+   <h1>
+       <img id="h1" src="../img/titulo.png">
+   </h1>
+
+   <nav>
+
+        <ul>
+            <li><a href="../index.php" class="botones"><img src="../img/inicio.png" class="iconos"></a></li>
+            <li><a href="audios.php" class="botones"><img src="../img/audios.png" class="iconos"></a></li>
+            <li><a href="video.php" class="botones"><img src="../img/video.png" class="iconos"></a></li>
+            <li><a href="acerca.php" class="botones"><img src="../img/acerca.png" class="iconos"></a></li>
+            
+        </ul>
+    
+    </nav>
+
+    <section>
+		
+		<div id="caja10">
+			
+		</div>
+		
+    	<div id="caja11">
+			
+		</div>
+		
+    	<div id="caja12">
+
+            
+        </div>
+		
+    	<div id="caja13">
+
+            <p class="juan">Mini biografía</p> 
+			
+			<p id="juan">
+                Me llamo Juan Guillermo Del Valle Urrego. 
+                Nací en Medellín Antioquia, el 11 de Abril de 1993, en
+                una familia de la cual estoy orgulloso. 
+                Crecí en el Valle del Cauca, donde viví los 
+                primeros 4 años. Actualmente me desempeño como
+                guitarrista y bajista. Me gustan los gatos, 
+                escribir canciones o poemas, amante de la lectura
+                y de los viajes a lugares mágicos. 
+            </p>
+
+           
+			
+		</div>
+    </section>
+    
+    <footer>
+        <img id="footer" src="../img/footer.png">        
+    </footer>
+
+    
+</body>
+</html>
---
+++ html/video.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juan Guillermo Del Valle Urrego</title>
+     <!--Estilo para desktop-->
+     <link href="../css/desktop.css" rel="stylesheet" type="text/css" media="only screen and (min-width:1025px)" />
+
+    <!--Estilo para tablet-->
+    <link href="../css/tablet.css" rel="stylesheet" type="text/css" media="only screen and (min-width:768px) and (max-width:1024px)" />
+
+    <!--Estilo para movil-->
+    <link href="../css/movil.css" rel="stylesheet" type="text/css" media="only screen and (min-width:280px) and (max-width:767px)" />
+<body>
+    <header>
+        <img src="../img/cabecera1.png" id="header">
+   </header>
+
+   <h1>
+       <img id="h1" src="../img/titulo.png">
+   </h1>
+
+   <nav>
+
+        <ul>
+            <li><a href="../index.php" class="botones"><img src="../img/inicio.png" class="iconos"></a></li>
+            <li><a href="audios.php" class="botones"><img src="../img/audios.png" class="iconos"></a></li>
+            <li><a href="video.php" class="botones"><img src="../img/video.png" class="iconos"></a></li>
+            <li><a href="acerca.php" class="botones"><img src="../img/acerca.png" class="iconos"></a></li>
+            
+        </ul>
+    
+    </nav>
+
+    <section>
+        <div id="caja9">
+
+            <h2 class="titulos">NextGen Studios</h2>
+            <p class="proyecto"> Proyecto NextGen Studios</p>
+            <p class="description1">
+                «Next-Gen Studios» fue una propuesta de trabajo realizada
+                    por algunos compañeros cuando estudiamos Electrónica General, 
+                    para la asignatura de emprendimiento. En el video aparece un 
+                    compañero y mi persona, interpretando en la guitarra pequeños
+                    fragmentos de canciones de rock y heavy metal. 
+
+                    El objetivo del video, era dar a conocer el talento de músicos
+                    y a su vez, crear un estudio de grabación en el 
+                    cual, las personas puedan desarrollar y potencializar sus 
+                    habilidades musicales, crear proyectos musicales, mezclas, 
+                    grabaciones, entre otros servicios. 
+            </p>
+            
+            <div class="video">
+                <video controls>
+                    <source src="../video/nextgen-studios.mp4" type="video/mp4">
+                    <source src="../video/nextgen-studios.ogv" type="video/ogv">
+                    <source src="../video/nextgen-studios.webm" type="video/webm">    
+
+                </video>
+
+           
+            </div>
+
+        </div>
+    </section>
+
+    <footer>
+        <img id="footer" src="../img/footer.png">        
+    </footer>
+
+   
+</body>
+</html>
